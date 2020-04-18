ae40a52f0fc2a173a8f90f0cc72ae9e1e720cea8
Mohammad Khan <mak.atif@gmail.com>
2020-04-18 20:19:18 +0300

    Add function to get masks (negative, annotation, neighboring) Modify neighboring masks function (reduce blank area) Add Label and GT Mask function Refactor patch coordinate generator function

--- Slide.py
+++ Slide.py
@@ -91,6 +91,9 @@ class Slide:
         else:
             self.annotation = Annotation(annot_file, scaleFactor=self.slide.level_downsamples[self.extraction_level])
 
+        self.mask_negatives = None
+        self.mask_neighboring = None
+        self.mask_annotations = None
         
 
     def getWSI(self):
@@ -137,7 +140,7 @@ class Slide:
 
     def getHED(self, img):
         '''
-        Return a channel scaled image in the HED, color deconvolution performed format
+        Return a scaled-channel image in the HED, color deconvolution performed format
         '''
         hed = rgb2hed(img)
         #hed_sc = np.zeros_like(hed)
@@ -176,21 +179,42 @@ class Slide:
         mask = cv2.fillPoly(canvas, self.annotation.coords_list, 255)
         return mask
 
-    def getAnnotationNeighborhoodMask(self, kernel_size=5):
+    def getAnnotationNeighborhoodMask(self, kernel_size_inland=5, kernel_size_coast=2):
         '''
-        Returns a binary mask image with non-zero values around the region of the annotation mask.
+        Return a mask image with non-zero values only around the region of the annotation mask.
 
-        The function perform an XOR operation between the Annotation mask and its Dilated version
+        Simplistically, the function perform an XOR operation between the Annotation mask and its Dilated version. 
+        Additional logical operation are performed to reduce the area in the blank spots (not containing specimen scan) - called coast and 
+        increase the area in the informative region (containing specimen scan) - called inland. 
         '''
         
-        kernel = np.ones((kernel_size,kernel_size), np.uint8)
+        kernel_inland = np.ones((kernel_size_inland, kernel_size_inland), np.uint8)
+        kernel_coast = np.ones((kernel_size_coast, kernel_size_coast), np.uint8)
+        otsu_mask = self.getOtsu()
 
         annotation_mask = self.getAnnotationMask()
-        annotation_mask_dilated = cv2.dilate(annotation_mask, kernel, iterations=1)
+        #annotation_mask_dilated = cv2.dilate(annotation_mask, kernel, iterations=1)
+
+        #n_mask = np.logical_xor(annotation_mask, annotation_mask_dilated)
+
+        annot_inland = np.logical_and(annotation_mask, otsu_mask)
+        annot_coast = np.logical_and(annotation_mask, np.logical_not(otsu_mask))
+
+        annot_inland_dilated = cv2.dilate(annot_inland.astype(np.uint8)*255, kernel_inland)
+        annot_coast_dilated = cv2.dilate(annot_coast.astype(np.uint8)*255, kernel_coast)
+
+        annotation_mask_dilated = np.logical_or(annot_inland_dilated, annot_coast_dilated)
 
         n_mask = np.logical_xor(annotation_mask, annotation_mask_dilated)
-        return 255*(n_mask.astype(np.uint8))
+        #n_mask_inland = np.logical_xor(annot_inland, annot_inland_dilated)
+        #n_mask_coast = np.logical_xor(annot_coast, annot_coast_dilated)
 
+        #n_mask = np.logical_or(n_mask_inland, n_mask_coast)
+        
+        n_mask = 255*(n_mask.astype(np.uint8))
+
+        return n_mask
+    
     def getDABMask(self, img=None, margins=(25, -25, 25, -35)):
         '''
         Returns the bit mask for ROI from the given RGB img, using DAB channel of the HED (deconvoluted) image
@@ -204,35 +228,57 @@ class Slide:
 
         return mask
 
-    def getPatchCoordList(self, thresh_method='HED', with_filename=True):
-        '''
-        Returns a list of coordinates in the slide image where useful data is expected to be present by using 
-        thresholding to remove blank or non-informative areas, at the given image level (extraction_level)
+    def getGTmask(self, coords, dims=(256,256)):
+        if self.annotation==None:
+            return np.zeros((dims[0], dims[1], 1), dtype=np.uint8)
 
-        This function gets a downsampled version of the slide image at level either the highest level or the one specified in self.extraction_level
-        Then, it applies a thresholding technique, 
-        either 'HED' (DAB channel of the color deconvoluted, rescaled image) or
-                'OTSU' (Otsu thresholded mask) or
-                'N_MASK' (Region around an annotation)
-        depending on the 'thresh_method' value.
-        Following the mask generation, the indices of all values that are non-zero are recorded (with_filename if true) and returned as a list  
-        '''
+        c_shifted = self.annotation.shift(origin=coords)
+        c_scaled = self.annotation.scale(c_shifted, self.slide.level_downsamples[self.extraction_level])
 
-        #img = self.getRegionFromSlide()
-        
-        ##TODO: Add Otsu thresholding method as option to extract point of interest
-        
+        mask = cv2.fillPoly(np.zeros((dims[0], dims[1], 1), dtype=np.uint8), c_scaled, (255))
+
+        return mask
+
+    def getLabel(self, coords, dims=(256,256)):
+        '''
+        Return [1.0, 0.0] if the region at the specified rectangulare area (dims) starting at given coordinates (coords) does not have a detection else, 
+        return [0.0, 1.0] if the region at the specified rectangulare area (dims) starting at given coordinates (coords) has a detection 
+        '''
+        if self.annotation==None:
+            return np.array([1.0, 0.0])
+        else:
+            detection = np.any(self.getGTmask(coords, dims))
+            label = np.array( [float(not detection), float(detection)] )
+            return label
+
+    def generateROIMasks(self, thresh_method='HED'):
+        '''
+        Calculate and store as object variables: 
+            *a mask of the area containing regions of interest in the slide (containing the specimen scan) while exlcuding any regions of positive detection and areas around it
+            *a mask of the annotated area (if available)
+            *a mask of the area surrounding the annotations 
+
+        The ROI selection is done based on the given thresh_method, which could be either 
+                'HED' (DAB channel of the color deconvoluted, rescaled image) or
+                'OTSU' (Otsu thresholded mask)
+        '''
         if thresh_method == 'HED':
-            mask = self.getDABMask()
+            mask_whole = self.getDABMask()
         elif thresh_method == 'OTSU':
-            mask = self.getOtsu()
-        elif thresh_method=='N_MASK':
-            mask = self.getAnnotationNeighborhoodMask()
+            mask_whole = self.getOtsu()
 
-        if mask==None:
-            print('No annotation object present.')
-            return None
+        if self.annotation == None:
+            self.mask_negatives = mask_whole
+            return
 
+        self.mask_annotations = self.getAnnotationMask()
+        self.mask_neighboring = self.getAnnotationNeighborhoodMask()
+
+        self.mask_negatives = np.logical_and(mask_whole, np.logical_not(np.logical_or(self.mask_annotations, self.mask_neighboring)))
+        self.mask_negatives = self.mask_negatives.astype(np.uint8)*255
+
+
+    def getNonZeroLocations(self, mask, with_filename=True):
         nzs = np.argwhere(mask)
         nzs = nzs * self.slide.level_downsamples[self.extraction_level]
         nzs = nzs.astype(np.int32)
@@ -246,3 +292,28 @@ class Slide:
             l.append([self.slide_file, nzs[i].tolist()])
 
         return l
+
+
+    def getPatchCoordList(self, thresh_method='HED', with_filename=True):
+        '''
+        Returns a list of coordinates in the slide image where useful data is expected to be present by using 
+        thresholding to remove blank or non-informative areas, at the given image level (extraction_level)
+
+        This function gets a downsampled version of the slide image at level either the highest level or the one specified in self.extraction_level
+        Then, it applies a thresholding technique which could be either 
+                'HED' (DAB channel of the color deconvoluted, rescaled image) or
+                'OTSU' (Otsu thresholded mask)
+        depending on the 'thresh_method' value.
+        Following the mask generation, the indices of all values that are non-zero are recorded (with_filename if true) and returned as a list  
+        '''
+
+        self.generateROIMasks(thresh_method=thresh_method)
+        
+        if self.annotation==None:
+            return [self.getNonZeroLocations(self.mask_negatives)]
+        else:
+            return [self.getNonZeroLocations(self.mask_negatives, with_filename), \
+                self.getNonZeroLocations(self.mask_annotations, with_filename), \
+                self.getNonZeroLocations(self.mask_neighboring, with_filename)]
+
+        
